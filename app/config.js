57728c91ecf857d3c037d74da298f3965c228501
David Olsan <davidolsan@Mars-Spiders.local>
2018-04-28 17:54:29 +0100

    melody's prototype

--- app/config.js
+++ app/config.js
@@ -4,8 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
-
+  serviceName: 'Manage your education and skills funding',
   // Default port that prototype runs on
   port: '3000',
 
